1e4bb186ce72278bf00e787ef8c1fc4bc85ff187
Ryder James <ryderthefirst@gmail.com>
2018-08-11 13:22:53 -0600

    Refactored search to be part of a form

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            searchValue: ""
+        }
+    }
+
+    handleChange(e) {
+        this.setState({ searchValue: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("trying to search", this.state.searchValue);
+    }
+
     render() {
         return (
             <div className="header">
                 {/* <img className="header__img" src="https://i.imgur.com/k7jAoH3.png"/> */}
                 <i className="header__icon fab fa-d-and-d"/>
-                <input className="header__search" type="text" placeholder="Search Spells..."/>
+                <form className="header__search-wrapper" onSubmit={() => this.handleSubmit()}>
+                    <input 
+                        className="header__search-wrapper__search" 
+                        type="text" 
+                        placeholder="Search Spells..." 
+                        value={this.state.searchValue} 
+                        onChange={this.handleChange.bind(this)}
+                    />
+                </form>
                 {this.props.children}
             </div>
         );
